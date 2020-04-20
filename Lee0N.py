99546ba8d507ffd000d84cfacdf47f305b8f028e
Puppet00 <55746456+Puppet00@users.noreply.github.com>
2020-04-20 18:41:19 +0300

    Update Lee0N.py

--- Lee0N.py
+++ Lee0N.py
@@ -31,7 +31,7 @@ print ('''
      **     **
        ***** Lee0N
 
-------------------------------- ''')
+--------------------------------- ''')
 
 useragents = ["Mozilla/5.0 (Android; Linux armv7l; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 Fennec/10.0.1","Mozilla/5.0 (Android; Linux armv7l; rv:2.0.1) Gecko/20100101 Firefox/4.0.1 Fennec/2.0.1","Mozilla/5.0 (WindowsCE 6.0; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
 "Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0",
