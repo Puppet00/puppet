92dcaa5d711e1fc3a47606a2ce3d2c71b7c3938a
Puppet00 <55746456+Puppet00@users.noreply.github.com>
2020-05-24 04:29:34 +0300

    Update StiFFDoSTor.py

--- StiFFDoSTor.py
+++ StiFFDoSTor.py
@@ -174,6 +174,48 @@ def keyboardInterruptHandler(signal, frame):
     print("KeyboardInterrupt (ID: {}) has been caught. Cleaning up...".format(signal))
     exit(0)
 
+def create_useragents(thread):
+	for _ in range(thread):
+		useragents.append(getuseragent())
+
+def getuseragent():
+    platform = Choice(['Macintosh', 'Windows', 'X11'])
+    if platform == 'Macintosh':
+        os  = Choice(['68K', 'PPC', 'Intel Mac OS X'])
+    elif platform == 'Windows':
+        os  = Choice(['Win3.11', 'WinNT3.51', 'WinNT4.0', 'Windows NT 5.0', 'Windows NT 5.1', 'Windows NT 5.2', 'Windows NT 6.0', 'Windows NT 6.1', 'Windows NT 6.2', 'Win 9x 4.90', 'WindowsCE', 'Windows XP', 'Windows 7', 'Windows 8', 'Windows NT 10.0; Win64; x64'])
+    elif platform == 'X11':
+        os  = Choice(['Linux i686', 'Linux x86_64'])
+    browser = Choice(['chrome', 'firefox', 'ie'])
+    if browser == 'chrome':
+        webkit = str(Intn(500, 599))
+        version = str(Intn(0, 99)) + '.0' + str(Intn(0, 9999)) + '.' + str(Intn(0, 999))
+        return 'Mozilla/5.0 (' + os + ') AppleWebKit/' + webkit + '.0 (KHTML, like Gecko) Chrome/' + version + ' Safari/' + webkit
+    elif browser == 'firefox':
+        currentYear = datetime.date.today().year
+        year = str(Intn(2020, currentYear))
+        month = Intn(1, 12)
+        if month < 10:
+            month = '0' + str(month)
+        else:
+            month = str(month)
+        day = Intn(1, 30)
+        if day < 10:
+            day = '0' + str(day)
+        else:
+            day = str(day)
+        gecko = year + month + day
+        version = str(Intn(1, 72)) + '.0'
+        return 'Mozilla/5.0 (' + os + '; rv:' + version + ') Gecko/' + gecko + ' Firefox/' + version
+    elif browser == 'ie':
+        version = str(Intn(1, 99)) + '.0'
+        engine = str(Intn(1, 99)) + '.0'
+        option = Choice([True, False])
+        if option == True:
+            token = Choice(['.NET CLR', 'SV1', 'Tablet PC', 'Win64; IA64', 'Win64; x64', 'WOW64']) + '; '
+        else:
+            token = ''
+        return 'Mozilla/5.0 (compatible; MSIE ' + version + '; ' + os + '; ' + token + 'Trident/' + engine + ')'
 
 
 def createData(url,metode):
