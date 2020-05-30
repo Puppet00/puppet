57b47d79e2d5aec27a9fd71cec90ac2d614c2c12
Puppet00 <55746456+Puppet00@users.noreply.github.com>
2020-05-30 20:15:28 +0300

    Update and rename HTTPDoProxy.py to HTTPDoSProxy.py

--- HTTPDoSProxy.py
+++ HTTPDoSProxy.py
@@ -9,21 +9,20 @@ import sys
 
 print (''' \033[91m
 --------------------------------
-      .---.        .-----------|
-     /     \  __  /    ------  |
-    / /     \(..)/    -----    |
-   //////   ' \/ `   ---       |
-  //// / // :    : ---         |
- // /   /  /`    '--           |
-//          //..\\              |
-       ====UU====UU====        |
-           '//||\\`             |
-             ''``              |
-           ASPARTIM            |
+      .---.        .-----------
+     /     \  __  /    ------  
+    / /     \(..)/    -----    
+   //////   ' \/ `   ---       
+  //// / // :    : ---         
+ // /   /  /`    '--           
+//          //..\\
+       ====UU====UU====        
+           '//||\\`             
+             ''``              
+           ASPARTIM            
 -------------------------------- \033[0m ''')
-print(" ")
+print("Default Port 80")
 print("\033[94mHTTP Proxy Dos Attack\033[0m")
-print(" ")
 
 useragents = ["Mozilla/5.0 (Android; Linux armv7l; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 Fennec/10.0.1","Mozilla/5.0 (Android; Linux armv7l; rv:2.0.1) Gecko/20100101 Firefox/4.0.1 Fennec/2.0.1","Mozilla/5.0 (WindowsCE 6.0; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
 "Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0",
