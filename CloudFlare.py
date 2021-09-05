6b2cb5890620d3b53e45aa6faf09428104de6c06
Puppet00 <55746456+Puppet00@users.noreply.github.com>
2021-09-05 16:17:32 +0300

    Update and rename ClouddFlare.py to CloudFlare.py

--- CloudFlare.py
+++ CloudFlare.py
@@ -2,10 +2,15 @@ import cloudscraper
 import requests
 import threading
 from time import sleep
-import os
+import os, sys
 
+print(" ")
+print("Otomatik Kurulum Yapılıyor Hiçbir Tuşa Basmayınız . . .")
+time.sleep(3)
 os.system("pip install cloudscraper")
 os.system("pip install requests")
+os.system("clear")
+print(" ")
 
 # check the target url
 def check_url(url):
